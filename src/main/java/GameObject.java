3d64fc1084381a83c1077f908ab5af957b28c34b
rod-0818 <rodleu0818@yahoo.com.tw>
2020-08-06 21:48:14 +0800

    08/26 優化修改Wall

---
+++ src/main/java/GameObject.java
@@ -0,0 +1,16 @@
+import java.awt.*;
+
+public abstract class GameObject {
+
+    private int x;
+    private int y;
+    private Image image;
+
+    public GameObject(int x, int y, Image image) {
+        this.x = x;
+        this.y = y;
+        this.image = image;
+    }
+
+    abstract void draw(Graphics g);
+}
--- src/main/java/Wall.java
+++ src/main/java/Wall.java
@@ -1,22 +1,21 @@
 import javax.swing.*;
 import java.awt.*;
 
-public class Wall {
-    private int x;
-    private int y;
+public class Wall extends GameObject{
+//    private int x;
+//    private int y;
     private boolean horizontal;
     private  int brick;
 
     private Image image;
 
-    public Wall(int x, int y, boolean horizontal, int brick) {
-        this.x = x;
-        this.y = y;
+    Wall(int x, int y, boolean horizontal, int brick,Image image) {
+        super(x, y, image);
         this.horizontal = horizontal;
         this.brick = brick;
-        image = new ImageIcon("assets\\images\\brick.png").getImage();
     }
 
+    @Override
     public void draw(Graphics g){
         if (horizontal){
             for(int i=0;i<brick;i++){
